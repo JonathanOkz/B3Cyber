a7b87c858e8b0f44bd36990556e5d9bdb8b93ad8
playzir <letankiste17627@gmail.com>
2025-02-11 15:05:51 +0100

    Ajout des tests pour les routes utilisateurs

---
+++ Branche-test/src/tests/test.js
@@ -0,0 +1,10 @@
+const request = require('supertest');
+const app = require('../server');
+
+describe('GET /users', () => {
+  it('should return a list of users', async () => {
+    const res = await request(app).get('/users');
+    expect(res.statusCode).toEqual(200);
+    expect(res.body).toEqual([{ id: 1, name: 'Alice' }, { id: 2, name: 'Bob' }]);
+  });
+});

79a740d12cbdc2127aed6f472234b7441a7cf5a6
gabriel <gabriel.michenaud@my-digital-school.org>
2025-02-11 16:43:13 +0100

    test gab 1

--- Branche-test/src/tests/test.js
+++ Branche-test/src/tests/test.js
@@ -8,3 +8,27 @@ describe('GET /users', () => {
     expect(res.body).toEqual([{ id: 1, name: 'Alice' }, { id: 2, name: 'Bob' }]);
   });
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const request = require('supertest');
+const app = require('../server');
+
+describe('GET /users', () => {
+  it('should return a list of users', async () => {
+    const res = await request(app).get('/users');
+    expect(res.statusCode).toEqual(200);
+    expect(res.body).toEqual([{ id: 1, name: 'Alice' }, { id: 2, name: 'Bob' }]);
+  });
+});

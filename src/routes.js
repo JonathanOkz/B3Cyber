3b3434076f05be9f76588d1bbc08f6e97def4a95
ZedKah7 <emilien.louzier@gmail.com>
2025-02-17 10:11:46 +0000

    routes.js

--- src/routes.js
+++ src/routes.js
@@ -2,11 +2,11 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/users', (req, res) => {
-	res.json([{ id: 1, name: 'Alice' }, {id: 2, name: 'Bob' }]);
+	res.json([{ id: 1, name: 'SWAN' }, {id: 2, name: 'Bob' }]);
 });
 
 router.get('/users-2', (req, res) => {
-	res.json([{ id: 1, name: 'Thomas' }, {id: 2, name: 'Bob' }]);
+	res.json([{ id: 1, name: 'Emilien' }, {id: 2, name: 'Bob' }]);
 });
 
 module.exports = router;

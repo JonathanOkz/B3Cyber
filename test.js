abe3f1fe320545a2a146c9f16e8ee3678afbbd46
Samyidj0 <samyidjop@gmail.com>
2025-02-17 10:26:40 +0100

    restore file test.js

--- test.js
+++ test.js
@@ -8,31 +8,3 @@ describe('GET /users', () => {
     expect(res.body).toEqual([{ id: 1, name: 'Alice' }, { id: 2, name: 'Bob' }]);
   });
 });
-
-<<<<<<< HEAD
-
-
-
-
-
-
-
-
-
-
-
-
-
-const request = require('supertest');
-const app = require('../server');
-
-describe('GET /users', () => {
-  it('should return a list of users', async () => {
-    const res = await request(app).get('/users');
-    expect(res.statusCode).toEqual(200);
-    expect(res.body).toEqual([{ id: 1, name: 'Alice' }, { id: 2, name: 'Bob' }]);
-  });
-});
-=======
-// TEST ALEX
->>>>>>> 3dca408 (TestAlex1)

980cc1109815dd9d11c0e2887136c238e152a5e8
Geron <txmmi.gerxn@gmail.com>
2025-02-17 11:56:41 +0100

    test

--- api.js
+++ api.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 
-app.get('/api/products-2', (req, res) => {
+app.get('/api/products-24', (req, res) => {
   res.json([{ id: 1, name: 'Product 1' }, { id: 2, name: 'Product 2' }]);
 });
 

1b6f41482b23a6174374128554eeee20565069ca
benjamin-krauss20 <126577390+benjamin-krauss20@users.noreply.github.com>
2025-02-17 10:38:30 +0100

    Create api.js

---
+++ api.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const app = express();
+
+app.get('/api/products', (req, res) => {
+  res.json([{ id: 1, name: 'Product 1' }, { id: 2, name: 'Product 2' }]);
+});
+
+module.exports = app;

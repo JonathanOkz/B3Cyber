250d0bb3b2d89dec6d7e453e60044996c2b466d2
Dev301295 <devanshi.vora@my-digital-school.org>
2025-02-17 11:29:29 +0100

    Update server95.js

--- server95.js
+++ server95.js
@@ -1,1 +1,9 @@
+const express = require('express');
+const app = express();
+
+app.get('/api/products', (req, res) => {
+  res.json([{ id: 1, name: 'Tom' }, { id: 2, name: 'Jerry' }]);
+});
+
+module.exports = app;
 

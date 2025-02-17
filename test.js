0fa167ef5078be7bb84833ae9a3bf145493d6032
playzir <letankiste17627@gmail.com>
2025-02-17 10:31:20 +0100

    Démerde toi sam

--- test.js
+++ test.js
@@ -1,7 +1,7 @@
 const request = require('supertest');
 const app = require('../server');
 
-describe('GET /users', () => {
+describe('GET /users-3', () => {
   it('should return a list of users', async () => {
     const res = await request(app).get('/users');
     expect(res.statusCode).toEqual(200);
